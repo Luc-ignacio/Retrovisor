aa45d982081001b936d83bb9d6bd433ea4871137
Lucas Ignacio <lucas@codecabin.com.au>
2025-09-11 09:56:19 +1000

    Add landing page

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-red-500">
-      Hello World
-    </h1>
-
-    <button class="btn btn-secondary">
-      See Map <Icon name="tabler:map" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
