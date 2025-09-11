0c2606e77526902b3dde8348d40ca0badd14352e
Lucas Ignacio <ignacio.hnd@hotmail.com>
2025-09-11 17:03:04 +1000

    Show login state and add sign out logic

--- app/pages/error.vue
+++ app/pages/error.vue
@@ -5,17 +5,6 @@ const error = route.query.error || "An unknown error occurred";
 </script>
 
 <template>
-  <!-- <div class="card bg-base-300 container mx-auto mt-4 min-h-72 text-center flex flex-col items-center justify-center gap-4">
-    <div role="alert" class="alert alert-error">
-      <span>Oh no! {{ error }}</span>
-    </div>
-
-    <NuxtLink to="/" class="btn btn-secondary items-center">
-      <p>Home</p>
-      <Icon name="tabler:arrow-left" size="32" />
-    </NuxtLink>
-  </div> -->
-
   <div class="hero bg-base-300 container mx-auto mt-4">
     <div class="hero-content text-center min-h-96">
       <div class="max-w-md">
