cc2cba9dc7e6e73305082ebf32702a0b03e098b9
Lucas Ignacio <lucas@codecabin.com.au>
2025-09-11 11:36:20 +1000

    Add typesafe env setup

--- app/components/Base/Navbar.vue
+++ app/components/Base/Navbar.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="navbar bg-accent text-accent-content">
-    <div class="navbar-start">
-      <NuxtLink to="/" class="btn btn-ghost text-xl">
-        Retrovisor
-      </NuxtLink>
-    </div>
+  <div class="flex p-4 bg-accent text-accent-content">
+    <div class="container mx-auto flex items-center justify-between">
+      <div>
+        <NuxtLink to="/" class="text-xl font-semibold">
+          Retrovisor
+        </NuxtLink>
+      </div>
 
-    <div class="navbar-end items-center gap-2">
-      <BaseThemeToggle />
+      <div class="flex items-center gap-2">
+        <BaseThemeToggle />
 
-      <a class="btn btn-primary">
-        Sign In With Github
-        <Icon name="tabler:brand-github" size="24" />
-      </a>
+        <a class="btn btn-primary">
+          Sign In With Github
+          <Icon name="tabler:brand-github" size="24" />
+        </a>
+      </div>
     </div>
   </div>
 </template>
+git
